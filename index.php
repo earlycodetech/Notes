b436fdf11d6554000484d9999435998a13324fbe
emma2625 <emmanuelodobo10@gmail.com>
2023-10-04 14:24:14 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notes</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+
+    <section class="min-vh-100" style="background-image: url(assets/images/main-bg.jpg); background-size: cover;">
+        <div class="min-vh-100 w-100 d-flex justify-content-center align-items-center">
+            <div class="card border-0 shadow-lg" style="min-width: 400px;">
+                <form action="" method="post">
+                    <h3 class="text-center py-2">
+                        Login
+                    </h3>
+
+                    <div class="card-body">
+                        <label for="email">Email:</label>
+                        <input type="email" id="email" class="form-control mb-3">
+
+                        <label for="passsword">Password:</label>
+                        <input type="password" id="password"  class="form-control mb-3">
+
+                        <div class="text-center mb-2">
+                            <button class="btn btn-primary">
+                                Login
+                            </button>
+                        </div>
+
+                        <p>
+                            Don't have an account yet? <a href="signup.php">Create an Account</a>
+                        </p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+    
+
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
