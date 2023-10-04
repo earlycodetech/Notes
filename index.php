7172fc243d9221ffd77d0bb7a55acafa9b1853fa
emma2625 <emmanuelodobo10@gmail.com>
2023-10-04 15:09:48 +0100

    Added HTACCESS

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notes</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 </head>
-<body>
 
+<body>
     <section class="min-vh-100" style="background-image: url(assets/images/main-bg.jpg); background-size: cover;">
         <div class="min-vh-100 w-100 d-flex justify-content-center align-items-center">
             <div class="card border-0 shadow-lg" style="min-width: 400px;">
@@ -21,7 +22,7 @@
                         <input type="email" id="email" class="form-control mb-3">
 
                         <label for="passsword">Password:</label>
-                        <input type="password" id="password"  class="form-control mb-3">
+                        <input type="password" id="password" class="form-control mb-3">
 
                         <div class="text-center mb-2">
                             <button class="btn btn-primary">
@@ -30,15 +31,16 @@
                         </div>
 
                         <p>
-                            Don't have an account yet? <a href="signup.php">Create an Account</a>
+                            Don't have an account yet? <a href="signup">Create an Account</a>
                         </p>
                     </div>
                 </form>
             </div>
         </div>
     </section>
-    
+
 
     <script src="assets/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
--- signup.php
+++ signup.php
@@ -5,38 +5,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notes</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
 </head>
 <body>
+
 
     <section class="min-vh-100" style="background-image: url(assets/images/main-bg.jpg); background-size: cover;">
         <div class="min-vh-100 w-100 d-flex justify-content-center align-items-center">
             <div class="card border-0 shadow-lg" style="min-width: 400px;">
-                <form action="" method="post">
+                <div class="alert alert-danger position-fixed end-0 animate__animated animate__fadeInRight" style="z-index: 9999; top: 25%;">
+                    <strong>This is an error</strong>
+                </div>
+
+                <form action="app/register_control" method="post">
                     <h3 class="text-center py-2">
                         Create an Account
                     </h3>
 
                     <div class="card-body">
                         <label for="name">Full Name:</label>
-                        <input type="text" id="name" class="form-control mb-3">
+                        <input type="text" name="name" id="name" class="form-control mb-3">
 
                         <label for="email">Email:</label>
-                        <input type="email" id="email" class="form-control mb-3">
+                        <input type="email" name="email" id="email" class="form-control mb-3">
 
                         <label for="passsword">Password:</label>
-                        <input type="password" id="password"  class="form-control mb-3">
+                        <input type="password" name="password" id="password"  class="form-control mb-3">
 
                         <label for="confirm">Confirm Password:</label>
-                        <input type="password" id="confirm"  class="form-control mb-3">
+                        <input type="password" id="confirm" name="confirm" class="form-control mb-3">
 
                         <div class="text-center mb-2">
-                            <button class="btn btn-primary">
+                            <button class="btn btn-primary" name="register">
                                 Sign Up
                             </button>
                         </div>
 
                         <p>
-                           Already have an account? <a href="index.php">Sign In </a>
+                           Already have an account? <a href="index">Sign In </a>
                         </p>
                     </div>
                 </form>
